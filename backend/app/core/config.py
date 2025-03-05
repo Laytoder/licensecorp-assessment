e50cc3f980755ec6d0db128fd8f537e436ac442d
Lakshya Gupta <laytoder@gmail.com>
2025-03-04 20:16:46 -0700

    added config for postgres and redis

---
+++ backend/app/core/config.py
@@ -0,0 +1,21 @@
+import os
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    # postgres config
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "admin")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "password")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "todo_db")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "postgres")
+    POSTGRES_PORT: int = int(os.getenv("POSTGRES_PORT", 5432))
+
+    # redis config
+    REDIS_HOST: str = os.getenv("REDIS_HOST", "redis")
+    REDIS_PORT: int = int(os.getenv("REDIS_PORT", 6379))
+    REDIS_DB: int = int(os.getenv("REDIS_DB", 0))
+    TASKS_CHANNEL: str = os.getenv("TASKS_CHANNEL", "tasks_channel")
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
